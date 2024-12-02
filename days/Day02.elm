979eb4c886d4f417f29bb93a11a0baf8859a25c5
Max Hille <mh@lambdasoup.com>
2024-12-02 16:26:49 +0100

    Day 2 - Part 2

--- days/Day02.elm
+++ days/Day02.elm
@@ -2,7 +2,6 @@ module Day02 exposing (calculatePart1, calculatePart2, parser, puzzle)
 
 import Parser exposing (Parser, Trailing(..))
 import Puzzle exposing (Puzzle, inputParser, listParser)
-import Set
 
 
 type alias Report =
@@ -16,6 +15,7 @@ type alias Level =
 type Direction
     = Descending
     | Ascending
+    | Undecided
 
 
 calculatePart1 : List Report -> Result String Int
@@ -25,15 +25,26 @@ calculatePart1 reports =
             Err "reports list is empty"
 
         r1 :: rest ->
-            part1Help r1 rest []
+            partHelp False r1 rest []
                 |> Result.map (List.filter identity >> List.length)
 
 
-part1Help : Report -> List Report -> List Bool -> Result String (List Bool)
-part1Help report reports safeties =
+calculatePart2 : List Report -> Result String Int
+calculatePart2 reports =
+    case reports of
+        [] ->
+            Err "reports list is empty"
+
+        r1 :: rest ->
+            partHelp True r1 rest []
+                |> Result.map (List.filter identity >> List.length)
+
+
+partHelp : Bool -> Report -> List Report -> List Bool -> Result String (List Bool)
+partHelp dampener report reports safeties =
     let
         safety =
-            safe report
+            safe dampener report
     in
     case safety of
         Err msg ->
@@ -45,53 +56,111 @@ part1Help report reports safeties =
                     Ok <| bool :: safeties
 
                 next :: rest ->
-                    part1Help next rest (bool :: safeties)
+                    partHelp dampener next rest (bool :: safeties)
 
 
-safe : Report -> Result String Bool
-safe levels =
+safe : Bool -> Report -> Result String Bool
+safe dampener levels =
     case levels of
         l1 :: l2 :: rest ->
-            let
-                direction =
-                    if l1 < l2 then
-                        Ascending
-
-                    else
-                        Descending
-            in
-            Ok <| safeHelp l1 direction (l2 :: rest)
+            Ok <| safeHelp dampener l1 l2 Undecided rest
 
         _ ->
             Err "report does not have enough levels to assert safety"
 
 
-safeHelp : Level -> Direction -> List Level -> Bool
-safeHelp last direction togo =
+safeHelp : Bool -> Level -> Level -> Direction -> List Level -> Bool
+safeHelp dampener prev2 prev1 direction togo =
+    let
+        step l1 l2 =
+            let
+                diff =
+                    l1 - l2
+            in
+            if abs diff <= 3 && abs diff > 0 then
+                Just <|
+                    if diff < 0 then
+                        Ascending
+
+                    else
+                        Descending
+
+            else
+                Nothing
+    in
     case togo of
         [] ->
             True
 
         next :: rest ->
-            case direction of
-                Ascending ->
-                    if Set.member (next - last) (Set.fromList [ 1, 2, 3 ]) then
-                        safeHelp next direction rest
+            let
+                step1 =
+                    step prev2 prev1
 
-                    else
-                        False
+                step2 =
+                    step prev1 next
+            in
+            case direction of
+                Undecided ->
+                    case ( step1, step2, dampener ) of
+                        ( Just _, Nothing, True ) ->
+                            safeHelp False prev2 next Undecided rest
+                                || safeHelp False prev1 next Undecided rest
+
+                        ( Nothing, Just _, True ) ->
+                            safeHelp False prev1 next Undecided rest
+                                || safeHelp False prev2 next Undecided rest
+
+                        ( Just direction1, Just direction2, _ ) ->
+                            if direction1 == direction2 then
+                                safeHelp dampener prev1 next direction2 rest
+
+                            else if dampener then
+                                safeHelp False prev1 next direction2 rest
+                                    || (case step prev2 next of
+                                            Just direction3 ->
+                                                safeHelp False prev2 next direction3 rest
+
+                                            Nothing ->
+                                                False
+                                       )
+                                    || safeHelp False prev2 prev1 direction1 rest
+
+                            else
+                                False
+
+                        ( _, _, _ ) ->
+                            False
+
+                Ascending ->
+                    case ( step1, step2, dampener ) of
+                        ( Just Ascending, Just Ascending, _ ) ->
+                            safeHelp dampener prev1 next direction rest
+
+                        ( Just Ascending, _, True ) ->
+                            safeHelp False prev2 prev1 direction rest
+                                || safeHelp False prev2 next direction rest
+
+                        ( _, Just Ascending, True ) ->
+                            safeHelp False prev1 next direction rest
+
+                        ( _, _, _ ) ->
+                            False
 
                 Descending ->
-                    if Set.member (last - next) (Set.fromList [ 1, 2, 3 ]) then
-                        safeHelp next direction rest
+                    case ( step1, step2, dampener ) of
+                        ( Just Descending, Just Descending, _ ) ->
+                            safeHelp dampener prev1 next direction rest
 
-                    else
-                        False
+                        ( Just Descending, _, True ) ->
+                            safeHelp False prev2 prev1 direction rest
+                                || safeHelp False prev2 next direction rest
 
+                        ( _, Just Descending, True ) ->
+                            safeHelp False prev1 next direction rest
 
-calculatePart2 : List Report -> Result String Int
-calculatePart2 _ =
-    Err "implement"
+                        ( _, _, _ ) ->
+                            False
 
 
 parser : Parser (List Report)
--- tests/Day02Test.elm
+++ tests/Day02Test.elm
@@ -30,6 +30,56 @@ suite =
                         |> Result.mapError Parser.deadEndsToString
                         |> Result.andThen calculatePart1
                     )
+        , test "Part 2 - Example 1" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            7 6 4 2 1
+                            1 2 7 8 9
+                            9 7 6 2 1
+                            1 3 2 4 5
+                            8 6 4 4 1
+                            1 3 6 7 9
+                            """
+                in
+                Expect.equal
+                    (Ok 4)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
+        , test "Part 2 - Debugging 1" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            87 90 91 92 96 93
+                            """
+                in
+                Expect.equal
+                    (Ok 1)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
+        , test "Part 2 - Debugging 2" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            84 83 80 81 80
+                            """
+                in
+                Expect.equal
+                    (Ok 1)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
         , test "parser" <|
             \_ ->
                 let
