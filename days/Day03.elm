7fd5aa43ea9e6c2996c36c8a12cb4596d1bb590d
Max Hille <mh@lambdasoup.com>
2024-12-03 11:12:16 +0100

    Day 3 - Part 2

--- days/Day03.elm
+++ days/Day03.elm
@@ -6,6 +6,8 @@ import Puzzle exposing (Puzzle, inputParser)
 
 type Op
     = Mul Int Int
+    | Dont
+    | Do
 
 
 calculatePart1 : List (List Op) -> Result String Int
@@ -16,14 +18,41 @@ calculatePart1 =
                 case op of
                     Mul x y ->
                         x * y
+
+                    _ ->
+                        0
             )
         >> List.sum
         >> Ok
 
 
 calculatePart2 : List (List Op) -> Result String Int
-calculatePart2 _ =
-    Err "not implemented"
+calculatePart2 =
+    List.concat
+        >> List.foldl
+            (\op acc ->
+                case op of
+                    Mul x y ->
+                        { acc
+                            | sum =
+                                acc.sum
+                                    + (if acc.enabled then
+                                        x * y
+
+                                       else
+                                        0
+                                      )
+                        }
+
+                    Do ->
+                        { acc | enabled = True }
+
+                    Dont ->
+                        { acc | enabled = False }
+            )
+            { sum = 0, enabled = True }
+        >> .sum
+        >> Ok
 
 
 parser : Parser (List (List Op))
@@ -46,6 +75,12 @@ opsParserHelp revOps =
                 |. Parser.symbol ","
                 |= Parser.int
                 |. Parser.symbol ")"
+        , Parser.backtrackable <|
+            Parser.succeed (Parser.Loop (Dont :: revOps))
+                |. Parser.symbol "don't()"
+        , Parser.backtrackable <|
+            Parser.succeed (Parser.Loop (Do :: revOps))
+                |. Parser.symbol "do()"
         , Parser.succeed (Parser.Loop revOps)
             |. Parser.chompIf (\_ -> True)
         , Parser.succeed ()
--- tests/Day03Test.elm
+++ tests/Day03Test.elm
@@ -25,7 +25,22 @@ suite =
                         |> Result.mapError Parser.deadEndsToString
                         |> Result.andThen calculatePart1
                     )
-        , test "parser" <|
+        , test "Part 2 - Example 1" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+                            """
+                in
+                Expect.equal
+                    (Ok 48)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
+        , test "parser - Part 1" <|
             \_ ->
                 let
                     input =
@@ -37,6 +52,21 @@ suite =
                 Expect.equal
                     (Ok
                         [ [ Mul 2 4, Mul 5 5, Mul 11 8, Mul 8 5 ]
+                        ]
+                    )
+                    (Parser.run parser input)
+        , test "parser - Part 2" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+                            """
+                in
+                Expect.equal
+                    (Ok
+                        [ [ Mul 2 4, Dont, Mul 5 5, Mul 11 8, Do, Mul 8 5 ]
                         ]
                     )
                     (Parser.run parser input)
