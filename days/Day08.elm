0f78e093f45a24361d14a16c2845979ca66757c4
Max Hille <mh@lambdasoup.com>
2024-12-08 15:11:48 +0100

    Day 8 - Part 2

--- days/Day08.elm
+++ days/Day08.elm
@@ -14,6 +14,16 @@ type Antenna
 
 calculatePart1 : Grid Antenna -> Result String Int
 calculatePart1 grid =
+    calculate grid False |> Ok
+
+
+calculatePart2 : Grid Antenna -> Result String Int
+calculatePart2 grid =
+    calculate grid True |> Ok
+
+
+calculate : Grid Antenna -> Bool -> Int
+calculate grid harmonics =
     grid
         |> Grid.positionedFold
             (\( pos, ant ) dict ->
@@ -35,42 +45,79 @@ calculatePart1 grid =
             )
             Dict.empty
         >> Dict.values
-        >> List.foldl (\antennas nodes -> Set.union (antinodes antennas) nodes) Set.empty
-        >> Set.filter (inBounds grid)
+        >> List.foldl
+            (\antennas nodes ->
+                let
+                    inBounds ( x, y ) =
+                        grid |> Grid.size >> (\{ xs, ys } -> x < xs && x >= 0 && y < ys && y >= 0)
+                in
+                Set.union (antinodes inBounds harmonics antennas) nodes
+            )
+            Set.empty
         >> Set.size
-        >> Ok
 
 
-inBounds : Grid a -> ( Int, Int ) -> Bool
-inBounds grid ( x, y ) =
-    grid |> Grid.size >> (\{ xs, ys } -> x < xs && x >= 0 && y < ys && y >= 0)
-
-
-antinodes : List ( Int, Int ) -> Set ( Int, Int )
-antinodes antennas =
+antinodes : (( Int, Int ) -> Bool) -> Bool -> List ( Int, Int ) -> Set ( Int, Int )
+antinodes inBounds harmonics antennas =
     cartesian antennas antennas
+        |> List.filter (\( xy1, xy2 ) -> xy1 /= xy2)
         |> List.foldl
             (\( ( x1, y1 ), ( x2, y2 ) ) acc ->
-                if x1 == x2 && y1 == y2 then
-                    acc
+                let
+                    ( dx, dy ) =
+                        ( x1 - x2, y1 - y2 )
+                in
+                antinodesHelp inBounds
+                    harmonics
+                    ( dx, dy )
+                    ( x1, y1 )
+                    ( x2, y2 )
+                    (if harmonics then
+                        0
 
-                else
-                    let
-                        dx =
-                            x1 - x2
-
-                        dy =
-                            y1 - y2
-                    in
-                    ( x1 + dx, y1 + dy ) :: ( x2 - dx, y2 - dy ) :: acc
+                     else
+                        1
+                    )
+                    Set.empty
+                    |> Set.union acc
             )
-            []
-        |> Set.fromList
+            Set.empty
 
 
-calculatePart2 : Grid Antenna -> Result String Int
-calculatePart2 =
-    Puzzle.notImplemented
+antinodesHelp : (( Int, Int ) -> Bool) -> Bool -> ( Int, Int ) -> ( Int, Int ) -> ( Int, Int ) -> Int -> Set ( Int, Int ) -> Set ( Int, Int )
+antinodesHelp inBounds harmonics ( dx, dy ) ( x1, y1 ) ( x2, y2 ) n acc =
+    let
+        resonance1 =
+            ( x1 + n * dx, y1 + n * dy )
+
+        resonance2 =
+            ( x2 - n * dx, y2 - n * dy )
+    in
+    case ( inBounds resonance1, inBounds resonance2 ) of
+        ( False, False ) ->
+            acc
+
+        ( True, False ) ->
+            if harmonics then
+                antinodesHelp inBounds harmonics ( dx, dy ) ( x1, y1 ) ( x2, y2 ) (n + 1) (acc |> Set.insert resonance1)
+
+            else
+                acc
+                    |> Set.insert resonance1
+
+        ( False, True ) ->
+            if harmonics then
+                antinodesHelp inBounds harmonics ( dx, dy ) ( x1, y1 ) ( x2, y2 ) (n + 1) (acc |> Set.insert resonance2)
+
+            else
+                acc |> Set.insert resonance2
+
+        ( True, True ) ->
+            if harmonics then
+                antinodesHelp inBounds harmonics ( dx, dy ) ( x1, y1 ) ( x2, y2 ) (n + 1) (acc |> Set.insert resonance1 |> Set.insert resonance2)
+
+            else
+                acc |> Set.insert resonance1 |> Set.insert resonance2
 
 
 parser : Parser (Grid Antenna)
--- tests/Day08Test.elm
+++ tests/Day08Test.elm
@@ -38,6 +38,74 @@ suite =
                         |> Result.mapError Parser.deadEndsToString
                         |> Result.andThen calculatePart1
                     )
+        , test "Part 2 - Example 1" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            ............
+                            ........0...
+                            .....0......
+                            .......0....
+                            ....0.......
+                            ......A.....
+                            ............
+                            ............
+                            ........A...
+                            .........A..
+                            ............
+                            ............
+                            """
+                in
+                Expect.equal
+                    (Ok 34)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
+        , test "Part 2 - Example 2" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            T.........
+                            ...T......
+                            .T........
+                            ..........
+                            ..........
+                            ..........
+                            ..........
+                            ..........
+                            ..........
+                            ..........
+                            """
+                in
+                Expect.equal
+                    (Ok 9)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
+        , test "Part 2 - Debug 1" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            .T..
+                            ....
+                            .T..
+                            ....
+                            """
+                in
+                Expect.equal
+                    (Ok 2)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
         , test "antinodes 1" <|
             \_ ->
                 let
@@ -46,7 +114,7 @@ suite =
                 in
                 Expect.equal
                     (Set.fromList [ ( 3, 1 ), ( 6, 7 ) ])
-                    (antinodes antennas)
+                    (antinodes (\_ -> True) False antennas)
         , test "antinodes 2" <|
             \_ ->
                 let
@@ -55,7 +123,7 @@ suite =
                 in
                 Expect.equal
                     (Set.fromList [ ( 3, 1 ), ( 6, 7 ) ])
-                    (antinodes antennas)
+                    (antinodes (\_ -> True) False antennas)
         , test "parser" <|
             \_ ->
                 let
