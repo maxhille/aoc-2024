861cba6531b4e02357c07cc055e8b665532958fb
Max Hille <mh@lambdasoup.com>
2024-12-18 14:01:15 +0100

    Day 15 - Part 2

--- days/Day15.elm
+++ days/Day15.elm
@@ -1,5 +1,7 @@
 module Day15 exposing (Direction(..), Item(..), calculatePart1, calculatePart2, parser, puzzle)
 
+import Array
+import Day08 exposing (Antenna(..))
 import Grid exposing (Grid, Pos)
 import Parser exposing ((|.), (|=), Parser, Trailing(..))
 import Puzzle exposing (Puzzle)
@@ -14,6 +16,8 @@ type Item
     | Empty
     | Robot
     | Box
+    | LeftBox
+    | RightBox
 
 
 type Direction
@@ -26,6 +30,41 @@ type Direction
 calculatePart1 : { warehouse : Warehouse, moves : List Direction } -> Result String Int
 calculatePart1 { warehouse, moves } =
     apply moves warehouse |> Result.map gpsSum
+
+
+calculatePart2 : { warehouse : Warehouse, moves : List Direction } -> Result String Int
+calculatePart2 { warehouse, moves } =
+    widen warehouse
+        |> apply moves
+        |> Result.map gpsSum
+
+
+widen : Warehouse -> Warehouse
+widen =
+    let
+        mapRow =
+            Array.toList
+                >> List.concatMap
+                    (\item ->
+                        case item of
+                            Wall ->
+                                [ Wall, Wall ]
+
+                            Empty ->
+                                [ Empty, Empty ]
+
+                            Robot ->
+                                [ Robot, Empty ]
+
+                            Box ->
+                                [ LeftBox, RightBox ]
+
+                            _ ->
+                                []
+                    )
+                >> Array.fromList
+    in
+    Array.map mapRow
 
 
 apply : List Direction -> Warehouse -> Result String Warehouse
@@ -49,44 +88,227 @@ apply directions warehouse =
 
 
 move : Direction -> Pos -> Warehouse -> Maybe Warehouse
-move direction ( x, y ) warehouse =
+move direction here warehouse =
     let
-        newPos =
+        ( x, y ) =
+            here
+
+        next =
             case direction of
                 Up ->
                     ( x, y - 1 )
-
-                Down ->
-                    ( x, y + 1 )
 
                 Left ->
                     ( x - 1, y )
 
                 Right ->
                     ( x + 1, y )
+
+                Down ->
+                    ( x, y + 1 )
     in
-    case Maybe.map2 Tuple.pair (Grid.get ( x, y ) warehouse) (Grid.get newPos warehouse) of
-        Just ( Robot, Empty ) ->
-            warehouse
-                |> Grid.set ( x, y ) Empty
-                |> Grid.set newPos Robot
-                |> Just
+    case Grid.get here warehouse of
+        Just Robot ->
+            case Maybe.map (\center -> ( center, direction )) (Grid.get next warehouse) of
+                Just ( Empty, _ ) ->
+                    warehouse
+                        |> Grid.set here Empty
+                        |> Grid.set next Robot
+                        |> Just
 
-        Just ( Box, Empty ) ->
-            warehouse
-                |> Grid.set newPos Box
-                |> Just
+                Just ( Box, _ ) ->
+                    warehouse
+                        |> move direction next
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Robot)
 
-        Just ( Robot, Box ) ->
-            warehouse
-                |> move direction newPos
-                |> Maybe.map (Grid.set ( x, y ) Empty)
-                |> Maybe.map (Grid.set newPos Robot)
+                Just ( RightBox, Left ) ->
+                    warehouse
+                        |> move direction ( x - 2, y )
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Robot)
 
-        Just ( Box, Box ) ->
-            warehouse
-                |> move direction newPos
-                |> Maybe.map (Grid.set newPos Box)
+                Just ( RightBox, Up ) ->
+                    warehouse
+                        |> move direction ( x - 1, y - 1 )
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Robot)
+
+                Just ( RightBox, Down ) ->
+                    warehouse
+                        |> move direction ( x - 1, y + 1 )
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Robot)
+
+                Just ( LeftBox, Right ) ->
+                    warehouse
+                        |> move direction ( x + 1, y )
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Robot)
+
+                Just ( LeftBox, Up ) ->
+                    warehouse
+                        |> move direction ( x, y - 1 )
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Robot)
+
+                Just ( LeftBox, Down ) ->
+                    warehouse
+                        |> move direction ( x, y + 1 )
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Robot)
+
+                _ ->
+                    Nothing
+
+        Just Box ->
+            case Grid.get next warehouse of
+                Just Empty ->
+                    warehouse
+                        |> Grid.set here Empty
+                        |> Grid.set next Box
+                        |> Just
+
+                Just Box ->
+                    warehouse
+                        |> move direction next
+                        |> Maybe.map (Grid.set here Empty)
+                        |> Maybe.map (Grid.set next Box)
+
+                _ ->
+                    Nothing
+
+        Just LeftBox ->
+            case direction of
+                Left ->
+                    case Grid.get next warehouse of
+                        Just RightBox ->
+                            warehouse
+                                |> move direction ( x - 2, y )
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set here RightBox)
+                                |> Maybe.map (Grid.set next LeftBox)
+
+                        Just Empty ->
+                            warehouse
+                                |> Grid.set ( x + 1, y ) Empty
+                                |> Grid.set here RightBox
+                                |> Grid.set next LeftBox
+                                |> Just
+
+                        _ ->
+                            Nothing
+
+                Right ->
+                    case Grid.get (next |> (\( x_, y_ ) -> ( x_ + 1, y_ ))) warehouse of
+                        Just LeftBox ->
+                            warehouse
+                                |> move direction ( x + 2, y )
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 2, y ) RightBox)
+
+                        Just Empty ->
+                            warehouse
+                                |> Grid.set ( x, y ) Empty
+                                |> Grid.set ( x + 1, y ) LeftBox
+                                |> Grid.set ( x + 2, y ) RightBox
+                                |> Just
+
+                        _ ->
+                            Nothing
+
+                Up ->
+                    case Maybe.map2 Tuple.pair (Grid.get next warehouse) (Grid.get (next |> (\( x_, y_ ) -> ( x_ + 1, y_ ))) warehouse) of
+                        Just ( RightBox, LeftBox ) ->
+                            warehouse
+                                |> move Up ( x - 1, y - 1 )
+                                |> Maybe.andThen (move direction ( x + 1, y - 1 ))
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y - 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y - 1 ) RightBox)
+
+                        Just ( LeftBox, _ ) ->
+                            warehouse
+                                |> move Up ( x, y - 1 )
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y - 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y - 1 ) RightBox)
+
+                        Just ( RightBox, Empty ) ->
+                            warehouse
+                                |> move Up ( x - 1, y - 1 )
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y - 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y - 1 ) RightBox)
+
+                        Just ( Empty, LeftBox ) ->
+                            warehouse
+                                |> move Up ( x + 1, y - 1 )
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y - 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y - 1 ) RightBox)
+
+                        Just ( Empty, Empty ) ->
+                            warehouse
+                                |> Grid.set ( x, y ) Empty
+                                |> Grid.set ( x + 1, y ) Empty
+                                |> Grid.set ( x, y - 1 ) LeftBox
+                                |> Grid.set ( x + 1, y - 1 ) RightBox
+                                |> Just
+
+                        _ ->
+                            Nothing
+
+                Down ->
+                    case Maybe.map2 Tuple.pair (Grid.get next warehouse) (Grid.get (next |> (\( x_, y_ ) -> ( x_ + 1, y_ ))) warehouse) of
+                        Just ( RightBox, LeftBox ) ->
+                            warehouse
+                                |> move direction ( x - 1, y + 1 )
+                                |> Maybe.andThen (move direction ( x + 1, y + 1 ))
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y + 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y + 1 ) RightBox)
+
+                        Just ( LeftBox, RightBox ) ->
+                            warehouse
+                                |> move direction ( x, y + 1 )
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y + 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y + 1 ) RightBox)
+
+                        Just ( Empty, LeftBox ) ->
+                            warehouse
+                                |> move direction ( x + 1, y + 1 )
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y + 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y + 1 ) RightBox)
+
+                        Just ( RightBox, Empty ) ->
+                            warehouse
+                                |> move direction ( x - 1, y + 1 )
+                                |> Maybe.map (Grid.set ( x, y ) Empty)
+                                |> Maybe.map (Grid.set ( x + 1, y ) Empty)
+                                |> Maybe.map (Grid.set ( x, y + 1 ) LeftBox)
+                                |> Maybe.map (Grid.set ( x + 1, y + 1 ) RightBox)
+
+                        Just ( Empty, Empty ) ->
+                            warehouse
+                                |> Grid.set ( x, y ) Empty
+                                |> Grid.set ( x + 1, y ) Empty
+                                |> Grid.set ( x, y + 1 ) LeftBox
+                                |> Grid.set ( x + 1, y + 1 ) RightBox
+                                |> Just
+
+                        _ ->
+                            Nothing
 
         _ ->
             Nothing
@@ -100,15 +322,13 @@ gpsSum =
                 Box ->
                     sum + x + 100 * y
 
+                LeftBox ->
+                    sum + x + 100 * y
+
                 _ ->
                     sum
         )
         0
-
-
-calculatePart2 : { warehouse : Warehouse, moves : List Direction } -> Result String Int
-calculatePart2 =
-    Puzzle.notImplemented
 
 
 parser : Parser { warehouse : Grid Item, moves : List Direction }
--- tests/Day15Test.elm
+++ tests/Day15Test.elm
@@ -70,6 +70,41 @@ suite =
                         |> Result.mapError Parser.deadEndsToString
                         |> Result.andThen calculatePart1
                     )
+        , test "Part 2 - Large example" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                            ##########
+                            #..O..O.O#
+                            #......O.#
+                            #.OO..O.O#
+                            #..O@..O.#
+                            #O#..O...#
+                            #O..O..O.#
+                            #.OO.O.OO#
+                            #....O...#
+                            ##########
+
+                            <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+                            vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+                            ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+                            <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+                            ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+                            ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+                            >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+                            <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+                            ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+                            v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+                            """
+                in
+                Expect.equal
+                    (Ok 9021)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
         , test "parser" <|
             \_ ->
                 let
